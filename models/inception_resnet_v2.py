8fb51656d19a2850c8d97e5ac41667f5712a9362
pharrell <pharrellyang90@gmail.com>
2018-01-29 16:27:19 +0800

    ignores .DS_Store

--- models/inception_resnet_v2.py
+++ models/inception_resnet_v2.py
@@ -3,3 +3,8 @@ Pytorch implementation for Inception ResNet v2.
 
 Original paper: https://arxiv.org/pdf/1602.07261.pdf
 '''
+
+import torch
+import torch.nn as nn
+
+from torch.autograd import Variable
